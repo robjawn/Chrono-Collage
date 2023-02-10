c954fcdec2b0c8da97169ee1074b760f30dc2118
Robert Johnson <robljohn1998@gmail.com>
2023-02-09 21:57:57 -0500

    further wip on models, now instantiating in database

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class PhotoContext(models.Model):
-    date = models.DateField()
+    date = models.CharField(max_length=20, blank=True, default='')
     description = models.TextField(max_length=500, blank=True, default='')
     location = models.CharField(max_length=200, blank=True, default='')
     people = models.CharField(max_length=200, blank=True, default='')
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
-
 from django.http import HttpResponse
+from .models import Photo, PhotoContext
 
 class Photo: 
   def __init__(self, title, date, description, url):
